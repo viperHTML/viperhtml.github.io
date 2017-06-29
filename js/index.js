b2bc1878975c73c8688a62876e21ba01c0ee31a2
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2017-06-29 14:57:18 +0100

    first site

---
+++ js/index.js
@@ -0,0 +1,48 @@
+// - - - - - - - - - - - - - - - - - - - - - - - - -
+// Oh, The Irony!
+// - - - - - - - - - - - - - - - - - - - - - - - - -
+// No hyperHTML is used here, because I've generated
+// this site statically, and I want it to be readable
+// by everyone, with or without JavaScript.
+// There is also zero dynamic content, beside a modal.
+// Documentation and Examples are Coming Soon
+// - - - - - - - - - - - - - - - - - - - - - - - - -
+document.addEventListener(
+  'DOMContentLoaded',
+  function () {
+
+    // local variables
+    var doc = this;
+    var $ = function (css) { return doc.querySelector(css); };
+
+    // bulma toggle menu functionality
+    $('.nav-toggle').addEventListener('click', function (e) {
+      var menu = this.nextElementSibling;
+      e.preventDefault();
+      this.classList.toggle('is-active');
+      menu.classList.toggle('is-active');
+    });
+
+    // bulma full height fix for iOS
+    addEventListener('orientationchange', fixHeight);
+    fixHeight();
+    function fixHeight() {
+      $('section').style.minHeight = doc.documentElement.clientHeight + 'px';
+    }
+
+    // bulma modal
+    $('.modal').addEventListener('click', function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+      this.classList.remove('is-active');
+    });
+    $('[href="#examples"]').addEventListener('click', showModal);
+    $('[href="#documentation"]').addEventListener('click', showModal);
+    function showModal(e) {
+      e.preventDefault();
+      $('.modal').classList.add('is-active');
+    }
+
+  },
+  {once: true}
+);
