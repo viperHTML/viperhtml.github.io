d0480854a440bb6e9f92be162a1ecf3b78cffe70
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2017-07-27 12:50:52 +0100

    added reachable examples section

--- js/index.js
+++ js/index.js
@@ -34,7 +34,7 @@ document.addEventListener(
     // bulma modal
     $('.modal').addEventListener('click', function (e) {
       e.stopPropagation();
-      if (!/^patreon|paypal$/.test(e.target.id)) {
+      if (!/^patreon|paypal|examples$/.test(e.target.id)) {
         e.preventDefault();
         this.classList.remove('is-active');
       }
