dcdac7cedd0f9bd19d0dc1675356ccff4d7ed8ce
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2017-08-06 18:26:54 +0100

    progress

--- hyperhtml/documentation/js/index.js
+++ hyperhtml/documentation/js/index.js
@@ -1,12 +1,12 @@
 addEventListener(
   'DOMContentLoaded',
   function () {
-    // hljs.configure({useBR: true});
     [].forEach.call(
       document.querySelectorAll('pre > code'),
       function (node) {
         dropUnnecessarySpaces(node);
         hljs.highlightBlock(node);
+        hljs.lineNumbersBlock(node);
       }
     );
     function dropUnnecessarySpaces(node) {
@@ -14,7 +14,7 @@ addEventListener(
       var spaces = /^(\s+)/.test(lines[1]) && RegExp.$1;
       node.textContent = lines.join('\n')
         .replace(new RegExp('^' + spaces, 'gm'), '')
-        .replace(/^\s+|\s$/g, '');
+        .replace(/^\s+|\s+$/g, '');
     }
   },
   {once: true}
