349e55ed1de40e2167e5378634e7f1b4711158d9
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2017-08-11 21:35:29 +0100

    basic documentation

--- hyperhtml/documentation/js/index.js
+++ hyperhtml/documentation/js/index.js
@@ -2,13 +2,26 @@ addEventListener(
   'DOMContentLoaded',
   function () {
     [].forEach.call(
-      document.querySelectorAll('pre > code'),
+      $('pre > code'),
       function (node) {
         dropUnnecessarySpaces(node);
         hljs.highlightBlock(node);
         hljs.lineNumbersBlock(node);
       }
     );
+    hash();
+    addEventListener('hashchange', hash);
+    function $(css) {
+      var split = css.split(':first');
+      return split.length < 2 ?
+        document.querySelectorAll(split[0]) :
+        document.querySelector(split.join(''));
+    }
+    function hash() {
+      if (hash.el) hash.el.className = '';
+      hash.el = $('a[href="' + (location.hash || '#introduction') + '"]:first');
+      if (hash.el) hash.el.className = 'is-active';
+    }
     function dropUnnecessarySpaces(node) {
       var lines = node.textContent.split('\n');
       var spaces = /^(\s+)/.test(lines[1]) && RegExp.$1;
