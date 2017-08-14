62a538ad9a1ac9c04b8e2b21e17454576facf4a2
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2017-08-14 08:35:48 +0100

    fixed highlight

--- hyperhtml/documentation/js/index.js
+++ hyperhtml/documentation/js/index.js
@@ -2,6 +2,10 @@
 addEventListener(
   'DOMContentLoaded',
   function () {
+    var
+      html = document.documentElement,
+      offsetHeight = html.offsetHeight
+    ;
     [].forEach.call(
       $('pre > code'),
       function (node) {
@@ -26,19 +30,19 @@ addEventListener(
       function highlight(el) {
         if (el.nodeName === 'H2') el = el.parentNode;
         var rect = el.getBoundingClientRect();
-        if ((rect.top >= 0) && (rect.bottom <= window.innerHeight)) {
+        if ((rect.top >= 0) && (rect.bottom <= offsetHeight)) {
           if (el.id) hashChange($('a[href="#' + el.id + '"]:first', menu));
           return true;
         }
       }
       function onresize() {
-        var cssText = [
+        offsetHeight = html.offsetHeight;
+        menu.style.cssText = [
           'position: fixed;',
           'width: ' + (menu.parentNode.offsetWidth - 20) + 'px;',
-          'max-height:' + (document.documentElement.clientHeight - 140) + 'px;',
+          'max-height:' + (html.clientHeight - 140) + 'px;',
           'overflow-y:auto;'
         ].join('');
-        menu.style.cssText = cssText;
       }
     }($('.menu:first')));
     function $(css, parent) {
