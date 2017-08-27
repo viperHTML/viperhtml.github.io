e0b61f2c59963ea712f1da5cff918c16338e1c38
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2017-08-27 13:08:51 +0100

    better Marko VS hyperHTML coparison

--- hyperhtml/examples/example/marko/counter-ce/hyper.js
+++ hyperhtml/examples/example/marko/counter-ce/hyper.js
@@ -1,6 +1,12 @@
 class HyperCounter extends HyperHTMLElement {
-  get style () {
-    return `
+  created() {
+    this.attachShadow({mode: 'open'});
+    this.setState({count: 0});
+  }
+  onclick() {
+    this.setState(prev => ({count: prev.count + 1}));
+  }
+  get style () { return `
     .count {
       color:#09c;
       font-size:3em;
@@ -10,15 +16,7 @@ class HyperCounter extends HyperHTMLElement {
       padding:0.5em;
     }`;
   }
-  created() {
-    this.attachShadow({mode: 'open'});
-    this.setState({count: 0});
-  }
-  onclick() {
-    this.setState(prev => ({count: prev.count + 1}));
-  }
-  render() {
-    return this.html`
+  render() { return this.html`
     <style>${this.style}</style>
     <div class=count>${this.state.count}</div>
     <button class=example-button onclick=${this}>
