f268b4f7654c2cadf0bf5720bfadd8f1e2ce355e
icebox <albertosantini@gmail.com>
2018-04-07 17:25:52 +0200

    Remove getWire from input list bindings example.

--- hyperhtml/examples/example/vue/bindings-list/hyper.js
+++ hyperhtml/examples/example/vue/bindings-list/hyper.js
@@ -1,20 +1,15 @@
 const App = {
   html: hyperHTML.bind(document.querySelector("#app")),
-  wires: [],
-  getWire(i) {
-    return (this.wires[i] || (this.wires[i] = hyperHTML.wire()));
-  },
-  data: {
-    inputs: ["a", "b", "c"]
-  },
+  data: {inputs: ["a", "b", "c"]},
   onInput(event, index) {
     this.data.inputs[index] = event.target.value;
     this.render();
   },
   render() {
-    this.html`${this.data.inputs.map((inp, index) => this.getWire(index)`
-      <input value="${inp}" oninput="${e => this.onInput(e, index)}"><br>
-    `)}${JSON.stringify(this.data.inputs)}`;
+    this.html`${this.data.inputs.map(
+      (inp, index) => hyperHTML.wire(this, ':' + index)`
+      <input value=${inp} oninput=${e => this.onInput(e, index)}><br>`
+    )}${JSON.stringify(this.data.inputs)}`;
   }
 };
 App.render();
